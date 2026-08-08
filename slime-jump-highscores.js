d4ebef579944aea7b4211c7319000d83990536e5
W0okie <297143671+funshirtsarenicetohave@users.noreply.github.com>
2026-08-08 14:35:29 +0200

    Bump version to 2.26

--- slime-jump-highscores.js
+++ slime-jump-highscores.js
@@ -11,7 +11,7 @@
     "sb_publishable_DDpbowG2-g6ZVios1OTrfA_DkUyh2TC";
 
   const TABLE = "slime_jump_highscores";
-  const GAME_VERSION = "2.25";
+  const GAME_VERSION = "2.26";
 
   function isConfigured() {
     return (
